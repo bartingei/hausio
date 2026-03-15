3be77add942e82695610243b1635eac6b2b25e71
bartingei <johnpaulkibet3@gmail.com>
2026-03-15 14:07:08 +0300

    update createsu with admin email

--- nyumbalink/backend/accounts/management/commands/createsu.py
+++ nyumbalink/backend/accounts/management/commands/createsu.py
@@ -1,15 +1,15 @@
 from django.core.management.base import BaseCommand
 from accounts.models import User
-import os
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if os.environ.get('CREATE_SUPERUSER'):
-            if not User.objects.filter(email='admin@hausio.com').exists():
-                User.objects.create_superuser(
-                    username='admin',
-                    email='admin@hausio.com',
-                    password='Hausio@Admin2024',
-                    role='admin',
-                )
-                self.stdout.write('Superuser created')
+        if not User.objects.filter(email='bartingeijohnpaul@gmail.com').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='bartingeijohnpaul@gmail.com',
+                password='B@rtingei4',
+                role='admin',
+            )
+            self.stdout.write('Superuser created')
+        else:
+            self.stdout.write('Superuser already exists')
